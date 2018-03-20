c7371cce39653a5e069f6d994b272cd4adaf360e
Lars Graubner <mail@larsgraubner.de>
2018-03-20 22:56:30 +0100

    adjust function

--- src/functions/api.js
+++ src/functions/api.js
@@ -1,13 +1,13 @@
-import differenceInDays from 'date-fns/difference_in_days'
+const differenceInDays = require('date-fns/difference_in_days')
 
 exports.handler = function(event, context, callback) {
   const diff = differenceInDays(new Date(), new Date(2018, 0, 1))
 
   callback(null, {
     statusCode: 200,
-    body: {
+    body: JSON.stringify({
       current: Math.abs(diff) % 5 + 1,
       next: Math.abs(diff) + 1 % 5 + 1
-    }
+    })
   })
 }
